ed9f4ce9bf394d0f867f0f081aad8976fb45a3b2
areshka515 <arikn97@gmail.com>
2022-10-19 20:13:30 +0300

    added pipeline

--- jenkins/buildTool.groovy
+++ jenkins/buildTool.groovy
@@ -1,9 +1,21 @@
+IMAGE_NAME = "simpleci"
+
 def preBuild() {
     echo "Pre build stage"
 }
 
 def buildDockerImage() {
-    echo "build Docker image"
+    echo "Building image..."
+    sh("docker build -t $IMAGE_NAME -f src/Dockerfile src")
+}
+
+def buildDockerImage() {
+    echo "Running image..."
+    sh("docker run $IMAGE_NAME")
+}
+
+def clearCache() {
+    sh("docker rmi -f $IMAGE_NAME")
 }
 
 return this
